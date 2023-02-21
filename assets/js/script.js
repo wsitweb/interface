ce23bbcf7795ba550615b4e130b56aae5be8d2c8
ROMAN <poman207575@gmail.com>
2023-02-21 19:02:30 +0200

    dev

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,31 @@ const navItems = document.querySelectorAll('nav .nav-item');
 const blocks = document.querySelectorAll('.blocks .block');
 const block = document.querySelectorAll('.block');
 const toggle = document.querySelector('.sidebar .toggle');
+const main = document.querySelector('main');
 let numTwo = 0;
 
-toggle.addEventListener('click', () => {
-     
-      if(sidebar.className === 'sidebar')
-            sidebar.classList.add('open'),  opacity();
-      else
-            sidebar.classList.remove('open'), opacity();
+if(window.innerWidth <= 768){
+      toggle.addEventListener('click', () => {
+      
+            if(sidebar.className === 'sidebar')
+                  sidebar.classList.add('open-mobil'), main.style.filter = 'blur(2px)';
+            else
+                  sidebar.classList.remove('open-mobil'), main.style.filter = 'blur(0px)';
 
-});
+      });
+
+
+}else{
+      toggle.addEventListener('click', () => {
+      
+            if(sidebar.className === 'sidebar')
+                  sidebar.classList.add('open');
+            else
+                  sidebar.classList.remove('open');
+
+      });
+}
+
 var navItem;
 navItems.forEach(navItem => {
       navItem.addEventListener('click', () => {
